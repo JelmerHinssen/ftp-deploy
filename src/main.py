b49ed6a33670174f93414b2727d1b6040044162b
Jelmer Hinssen <jelmerhinssen@hotmail.com>
2024-02-11 20:30:51 +0100

    compute local sha256sums

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
-# src/get_num_square.py
 from dataclasses import *
 import os
+from ftplib import *
+import hashlib
 
 def env(name: str, default=None) -> str:
     val = os.getenv(name, default)
@@ -26,3 +27,37 @@ class Arguments:
 
 args = Arguments()
 print(f"Action called with {repr(args)}")
+
+BUF_SIZE = 65536
+
+def sha256(path):
+    result = hashlib.sha256()
+    with open(path, "rb") as f:
+        while data := f.read(BUF_SIZE):
+            result.update(data)
+    return result.hexdigest()
+
+def filedata(entry: os.DirEntry):
+    result = {}
+    stat = entry.stat()
+    result["size"] = stat.st_size
+    result["sha256"] = sha256(entry.path)
+    return result
+
+def create_file_list(dir, root=False):
+    result = {"files":{}, "directories":{}}
+    for entry in os.scandir(dir):
+        print(entry.name)
+        if entry.is_file():
+            result["files"][entry.name] = filedata(entry)
+        elif entry.is_dir():
+            if not root or entry.name != "deploy-data":
+                result["directories"][entry.name] = create_file_list(entry.path)
+    return result
+
+
+print(create_file_list(args.local_dir, True))
+
+# with FTP(args.server, args.username, args.password, timeout=10) as ftp:
+#     print(ftp.retrlines("LIST"))
+#     print(ftp.retrlines("SITE HELP"))
