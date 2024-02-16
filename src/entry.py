e2905a313270549a601eb391b1af958f067ae5af
Jelmer Hinssen <jelmerhinssen@hotmail.com>
2024-02-16 08:26:58 +0100

    Create and retrieve file lists

---
+++ src/entry.py
@@ -0,0 +1,91 @@
+import json
+import dataclasses as dc
+import types
+import typing
+import typeguard
+
+
+class EnhancedJSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        if dc.is_dataclass(o):
+            return dc.asdict(
+                o, dict_factory=lambda x: {k: v for (k, v) in x if v is not None}
+            )
+        return super().default(o)
+
+
+def check_type(val, expected_type):
+    if isinstance(expected_type, str):
+        expected_type = eval(expected_type)
+    if typing.get_origin(expected_type) is types.UnionType:
+        expected_type = typing.get_args(expected_type)
+    if typing.get_origin(expected_type) is None:
+        return isinstance(val, expected_type)
+    else:
+        try:
+            typeguard.check_type(val, expected_type)
+            return True
+        except typeguard.TypeCheckError:
+            return False
+
+class EntryJSONDecoder(json.JSONDecoder):
+    def __init__(self, *args, **kwargs):
+        json.JSONDecoder.__init__(self, object_hook=self.object_hook, *args, **kwargs)
+    def object_hook(self, obj: dict):
+        if 'type' in obj:
+            if obj["type"] == "file":
+                cls_type = FileEntry
+            elif obj["type"] == "dir":
+                cls_type = DirectoryEntry
+            else:
+                cls_type = None
+            if cls_type is not None:
+                cls_fields: typing.Tuple[dc.Field, ...] = dc.fields(cls_type)
+                init_dict = {}
+                after_dict = {}
+                for x in cls_fields:
+                    required = not (x.default != dc._MISSING_TYPE and x.default_factory != dc._MISSING_TYPE)
+                    has_val = x.name in obj
+                    if has_val:
+                        val = obj[x.name]
+                        if not check_type(val, x.type):
+                            raise RuntimeError(f"Type of {x.name} ({type(obj[x.name])}) does not match requirement {x.type}")
+                                
+                    if x.init:
+                        if has_val:
+                            init_dict[x.name] = val
+                        elif required:
+                            raise RuntimeError(f"Missing required field {x.name} for {cls_type}")
+                    elif has_val:
+                        after_dict[x.name] = val
+                result = cls_type(**init_dict)
+                for field in after_dict:
+                    setattr(result, field, after_dict[field])
+                return result
+        return obj
+
+
+@dc.dataclass
+class Entry:
+    type: str = dc.field(init=False)
+    changed: str | None = dc.field(default=None, init=False)
+    modified: str | None = dc.field(default=None, init=False)
+
+
+@dc.dataclass
+class FileEntry(Entry):
+    name: str
+    size: int | None = None
+    sha256: str | None = None
+
+    def __post_init__(self):
+        self.type = "file"
+
+
+@dc.dataclass
+class DirectoryEntry(Entry):
+    name: str
+    content: dict[str, Entry] = dc.field(default_factory=dict)
+
+    def __post_init__(self):
+        self.type = "dir"
--- src/main.py
+++ src/main.py
@@ -1,13 +1,19 @@
-from dataclasses import *
+from contextlib import contextmanager
+import dataclasses as dc
 import os
-from ftplib import *
+import ftplib
 import hashlib
+import json
+from entry import *
+import typing
+
 
 def env(name: str, default=None) -> str:
     val = os.getenv(name, default)
     if val is None:
         raise RuntimeError(f"No value provided for required {name}")
     return val
+
 
 def env_int(name: str, default=None) -> int:
     val = env(name, default)
@@ -16,19 +22,23 @@ def env_int(name: str, default=None) -> int:
     except:
         raise RuntimeError(f"Given {name} is not a valid integer")
 
-@dataclass
+
+@dc.dataclass
 class Arguments:
     server: str = env("INPUT_SERVER")
     username: str = env("INPUT_USERNAME")
-    password: str = field(default=env("INPUT_PASSWORD"), repr=False)
+    password: str = dc.field(default=env("INPUT_PASSWORD"), repr=False)
     port: int = env_int("INPUT_PORT", 21)
     local_dir: str = env("INPUT_LOCAL_DIR", "./")
-    server_dir: str = env("INPUT_SERVER_DIR", "./")
+    server_dir: str = env("INPUT_SERVER_DIR", "/")
+    data_file: str = env("INPUT_DATA_FILE", "deploy-data/files.json")
+
 
 args = Arguments()
 print(f"Action called with {repr(args)}")
 
 BUF_SIZE = 65536
+
 
 def sha256(path):
     result = hashlib.sha256()
@@ -37,27 +47,122 @@ def sha256(path):
             result.update(data)
     return result.hexdigest()
 
-def filedata(entry: os.DirEntry):
-    result = {}
+
+def filedata(entry: os.DirEntry) -> FileEntry:
     stat = entry.stat()
-    result["size"] = stat.st_size
-    result["sha256"] = sha256(entry.path)
-    return result
+    return FileEntry(entry.name, size=stat.st_size, sha256=sha256(entry.path))
 
-def create_file_list(dir, root=False):
-    result = {"files":{}, "directories":{}}
+
+local_dir = os.path.normpath(args.local_dir)
+local_data_file = os.path.normpath(os.path.join(local_dir, args.data_file))
+remote_data_file = os.path.normpath(os.path.join(args.server_dir, args.data_file))
+
+
+def create_file_list(dir, exclude=[]) -> dict[str, Entry]:
+    result: dict[str, Entry] = {}
     for entry in os.scandir(dir):
-        print(entry.name)
+        path = os.path.normpath(entry.path)
+        if path in exclude:
+            continue
         if entry.is_file():
-            result["files"][entry.name] = filedata(entry)
+            result[entry.name] = (filedata(entry))
         elif entry.is_dir():
-            if not root or entry.name != "deploy-data":
-                result["directories"][entry.name] = create_file_list(entry.path)
+            result[entry.name] = DirectoryEntry(entry.name, create_file_list(entry.path, exclude))
     return result
 
+def split_path(path: str) -> list[str]:
+    parts: list[str] = []
+    while len(path) > 0:
+        path, tail = os.path.split(path)
+        if len(tail) == 0: break
+        parts.append(tail)
+        print(path, tail)
+    parts.reverse()
+    return parts
 
-print(create_file_list(args.local_dir, True))
+@contextmanager
+def cwd_single(ftp: ftplib.FTP, dir: str):
+    ftp.cwd(dir)
+    print(f"cd {dir}")
+    try:
+        yield
+    finally:
+        ftp.cwd("..")
+        print(f"cd ..")
 
-# with FTP(args.server, args.username, args.password, timeout=10) as ftp:
-#     print(ftp.retrlines("LIST"))
-#     print(ftp.retrlines("SITE HELP"))
+@contextmanager
+def cwd_parts(ftp: ftplib.FTP, parts: list[str]):
+    if len(parts) == 0:
+        yield
+    else:
+        with cwd_single(ftp, parts[0]):
+            with typing.cast(typing.ContextManager, cwd_parts(ftp, parts[1:])):
+                yield
+
+@contextmanager
+def cwd(ftp: ftplib.FTP, dir):
+    parts = split_path(dir)
+    with cwd_parts(ftp, parts):
+        yield
+
+def create_remote_file_list(ftp: ftplib.FTP, exclude=[]) -> dict[str, Entry]:
+    result: dict[str, Entry] = {}
+    for name, info in ftp.mlsd():
+        path = os.path.normpath(os.path.join(ftp.pwd(), name))
+        if path in exclude:
+            continue
+        if info["type"] == "file":
+            file = FileEntry(name, size=int(info["size"]))
+            file.modified = info["modify"]
+            result[name] = file
+        elif info["type"] == "dir":
+            with cwd(ftp, name):
+                result[name] = DirectoryEntry(name, create_remote_file_list(ftp, exclude))
+    return result
+
+def retrieve_file(ftp: ftplib.FTP, path: str) -> bytes:
+    result = b""
+    def retrieve(data: bytes):
+        nonlocal result
+        result += data
+    path, file = os.path.split(path)
+    with cwd(ftp, path):
+        print(ftp.pwd())
+        ftp.dir()
+        print(f"RETR {file}")
+        ftp.retrbinary(f"RETR {file}", retrieve)
+    return result
+
+def retrieve_remote_file_list(ftp: ftplib.FTP):
+    try:
+        return json.loads(retrieve_file(ftp, remote_data_file).decode(), cls=EntryJSONDecoder)
+    except:
+        return {}
+
+with ftplib.FTP(args.server, args.username, args.password, timeout=10) as ftp:
+    retrieved = retrieve_remote_file_list(ftp)
+    with cwd(ftp, os.path.normpath(args.server_dir)):
+        remote = create_remote_file_list(ftp, [remote_data_file])
+
+local = create_file_list(os.path.normpath(args.local_dir), [local_data_file])
+
+obj = {"retrieved": retrieved, "remote": remote, "local": local}
+
+with open("files.json", "w") as f:
+    json.dump(obj, f, indent="  ", cls=EnhancedJSONEncoder)
+
+
+# def file_diff(a, b):
+#     added = {"files": {}, "directories": {}}
+#     removed = {"files": {}, "directories": {}}
+#     updated = {"files": {}, "directories": {}}
+#     for name in a["files"]:
+#         if name not in b["files"]:
+#             added["files"][name] = a["files"][name]
+#         elif True:
+#             pass
+#     for name in a["directories"]:
+#         if name not in b["directories"]:
+#             added["directories"][name] = a["directories"]["name"]
+#         else:
+#             pass
